4be0ca009e8eab7925fd6cceaf9dcd474a1aec59
dcDalin <mcdalinoluoch@gmail.com>
2019-08-29 22:20:36 +0300

    add default url

--- src/index.js
+++ src/index.js
@@ -14,6 +14,14 @@ const { PORT } = process.env;
 
 app.use(bodyParser.json());
 
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .redirect(
+      'https://documenter.getpostman.com/preview/3662708-ad4e72d0-0a38-4d17-8cb8-fcf4c4416e7c?versionTag=latest&apiName=CURRENT&version=latest&top-bar=ffffff&right-sidebar=303030&highlight=ef5b25#4b707fdf-d1da-4cd0-a26a-a4f9bc4f5414',
+    );
+});
+
 app.use('/api/county', countyRouter);
 app.use('/api/sub-county', subCountyRouter);
 
